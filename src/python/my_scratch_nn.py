66da81d1a56fd9218e71764efeb60fbdefe36b4d
ais-lab <bariscankurtkaya@gmail.com>
2022-07-26 17:57:13 +0200

    equations added

--- src/python/my_scratch_nn.py
+++ src/python/my_scratch_nn.py
@@ -8,6 +8,8 @@ class Scratch_NN:
 
         self.init_data(self.data_dir)
 
+        self.math_eq = Math_Equations()
+
        
         
     def init_data(self, direction):
@@ -30,9 +32,26 @@ class Scratch_NN:
         self.pixels_count = self.class_plus_pixels_count - 1
 
     def init_params(self):
-        
+        self.W1 = np.random.rand(10,784) - 0.5
+        self.b1 = np.random.rand(10, 1) - 0.5
+        self.W2 = np.random.rand(10, 10) - 0.5
+        self.b2 = np.random.rand(10, 1) - 0.5
+
+
+class Math_Equations:
+    def __init__(self):
+        continue
+
+    def ReLU(Z):
+        return np.maximum(Z, 0)
+
+    def softmax(Z):
+        A = np.exp(Z) / sum(np.exp(Z))
+        return A
+
+    def ReLU_deriv(Z):
+        return Z > 0
 
 
 if __name__ == "__main__":
     scratch__nn = Scratch_NN()
-    
